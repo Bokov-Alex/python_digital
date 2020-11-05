1063849ef5fbcb4d7de3712fb1df6ba039373ed0
AL-X <thealex111@gmail.com>
2020-11-05 22:12:28 +0200

    middle exercises

---
+++ Lesson1/Middle Exercises.py
@@ -0,0 +1,37 @@
+import datetime
+# exercise 1
+print("exercise 1\n---------------")
+print("Net4U, is the best place\n      …in the world")
+
+#  exercise 2
+print("exercise 2\n---------------")
+print("Current date and time :\n" + str(datetime.datetime.now()))
+
+# exercise 3
+# print("exercise 3\n---------------")
+# print(' '.join(input("enter your full name:")[::-1]))
+
+# exercise 4
+print("exercise 4\n---------------")
+# print(float(input("enter a file name:"))%.1f)
+
+# exercise 5
+print("exercise 5\n---------------")
+# exercise 6
+print("exercise 6\n---------------")
+# exercise 7
+print("exercise 7\n---------------")
+# exercise 8
+print("exercise 8\n---------------")
+# exercise 9
+print("exercise 9\n---------------")
+# exercise 10
+print("exercise 10\n---------------")
+# exercise 11
+print("exercise 11\n---------------")
+# exercise 12
+print("exercise 12\n---------------")
+# exercise 13
+print("exercise 13\n---------------")
+# exercise 14
+print("exercise 14\n---------------")
