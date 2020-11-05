3955fb961bd8653d6aea0fbacf93f6d696a24db9
AL-X <thealex111@gmail.com>
2020-11-05 21:02:39 +0200

    LAB3

---
+++ Lesson1/LAB3.py
@@ -0,0 +1,10 @@
+name="Alex Bokov"
+email="thealex111@gmail.com"
+age=23
+
+print("Full name:" + name + "\nemail:" + email + "\nage:" + str(age) + "\n\n")
+
+
+print("name backwards:" + name[::-1] + "\nage*3:" + str(age*3) + "\n\n")
+
+print(name[:5] in "idan ben dudu yuval shimon Alex yael gal adam shahar yana")
